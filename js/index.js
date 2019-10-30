01583c88359c1a09ab2e7fde63a2cea90d044694
banditslay3r <declan.drumming@gmail.com>
2019-10-30 00:37:31 +0000

    Random House Display Index

---
+++ js/index.js
@@ -0,0 +1,66 @@
+//When the document is ready this will run
+$(document).ready(function(e) {
+	load_property();
+});
+
+function load_property() {
+	// This is running an AJAX Request to get the property content
+	$.ajax({
+		url: `php/index_display.php`, // This is the api we are calling
+		method: `get`, // This is the method we are using
+		before: function(e) {
+			// This runs before the page is loaded
+			console.log('search_start');
+		},
+		//Once a result is returned the success is run
+		success: function(pre_result) {
+			let result = $.parseJSON(pre_result);
+			console.log(result);
+			$('#bedrooms').html(`${result[0].Bedrooms}`);
+			$('#bathrooms').html(`${result[0].Bathrooms}`);
+			$('#lounge').html(`${result[0].Lounge}`);
+			$('#dinning').html(`${result[0].Dinning}`);
+			$('#garage').html(`${result[0].Garage}`);
+			$('#carparks').html(`${result[0].Carparks}`);
+			$('#property-house').attr(
+				'src',
+				`dashboard/assets/img/property_thumb/${result[0].Thumbnail}`
+			); // End Property
+			$('#addr').html(`${result[0].Addr},`);
+			$('#addr').append(` ${result[0].Suburb},`);
+			$('#addr').append(` ${result[0].City},`);
+			$('#addr').append(` ${result[0].Post}.`);
+			$('#description').html(`${result[0].Description}`);
+			$('#sale-type').html(`${result[0].SaleType}`);
+
+			// Agent Data
+			$('#agent-thumbnail').attr(
+				'src',
+				`dashboard/assets/img/agent_thumb/${result[1].Agent_Thumbnail}`
+			);
+			$('#agent-name').html(`${result[1].Agent_Name}`);
+			$('#agent-mobile').append(`${result[1].Agent_Mobile}`);
+			$('#agent-work').append(`${result[1].Agent_Work}`);
+
+			// Company Data
+			$('#company-name').append(`${result[2].Company_Name}`);
+			$('#company-thumbnail').attr(
+				'src',
+				`dashboard/assets/img/company_thumb/${result[2].Company_Thumbnail}`
+			);
+
+			// Specifications
+			$('#land-area').prepend(`${result[0].Landarea}`);
+			$('#suburb').html(`${result[0].Suburb}`);
+			$('#city').html(`${result[0].City}`);
+			$('#region').html(`${result[0].Region}`);
+
+			//wishlist button
+			$('.wishlist_add_button')
+				.append(`<div class="col-12" style="text-align:center">
+            <button class="btn btn-primary" onclick="add_to_wishlist(${house_id})">Add To Wishlist</button>
+            </div>
+            `);
+		}
+	});
+}
---
+++ php/index_display.php
@@ -0,0 +1,91 @@
+<?php
+
+include_once 'config.php'; // include the database connect config
+
+$query = "SELECT * FROM Houses ORDER BY RAND() LIMIT 1"; // selects all from 'houses' where House_ID = id
+$result = mysqli_query($db, $query); // returns result
+
+$return_arr = array(); // create the return array
+
+while ($row = mysqli_fetch_assoc($result)) { // fetches all rows from 'houses' where House_ID = id
+  $Bedrooms = $row['Bedrooms']; // makes 'Bedrooms' row content into a variable
+  $LandArea = $row['Landarea'];
+  $Bathrooms = $row['Bathrooms'];
+  $Garage = $row['Garage'];
+  $Carparks = $row['Carparks'];
+  $Dinning = $row['Dinning'];
+  $Lounge = $row['Lounge'];
+  $SaleType = $row['SaleType'];
+  $Closing_Date = $row['Closing_Date'];
+  $Addr = $row['Addr'];
+  $Suburb = $row['Suburb'];
+  $City = $row['City'];
+  $Post = $row['Post'];
+  $Region = $row['Region'];
+  $Description = $row["Description"];
+  $Thumbnail = $row["Thumbnail"];
+  $House_ID = $row["House_ID"];
+
+  array_push($return_arr, array( // adds below text to empty array "$return_arr" 
+    'Bedrooms' => $Bedrooms,
+    'Landarea' => $LandArea,
+    'Bathrooms' => $Bathrooms,
+    'Garage' => $Garage,
+    'Carparks' => $Carparks,
+    'Dinning' => $Dinning,
+    'Lounge' => $Lounge,
+    'SaleType' => $SaleType,
+    'Closing_date' => $Closing_Date,
+    'Addr' => $Addr,
+    'Suburb' => $Suburb,
+    'City' => $City,
+    'Post' => $Post,
+    'Region' => $Region,
+    'Description' => $Description,
+    'Thumbnail' => $Thumbnail));
+
+}
+
+$query = "SELECT * FROM Agent_Houses WHERE House_ID ='$House_ID' "; // selects all from 'Agent_Houses' where House_ID = id
+$result = mysqli_query($db, $query); // returns result
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $Agent_ID = $row['Agent_ID'];
+}
+
+$query = "SELECT * FROM Agents WHERE Agent_ID ='$Agent_ID' ";
+$result = mysqli_query($db, $query); // returns result
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $Agent_ID = $row['Agent_ID'];
+    $Company_ID = $row['Company_ID'];
+    $Agent_Thumbnail = $row['Thumbnail'];
+    $Agent_Name = $row['Agent_Name'];
+    $Agent_Work = $row['Agent_Work'];
+    $Agent_Mobile = $row['Agent_Mobile'];
+
+array_push($return_arr, array(
+    'Agent_ID' => $Agent_ID,
+    'Company_ID' => $Company_ID,
+    'Agent_Thumbnail' => $Agent_Thumbnail,
+    'Agent_Name' => $Agent_Name,
+    'Agent_Work' => $Agent_Work,
+    'Agent_Mobile' => $Agent_Mobile));
+
+}
+
+$query ="SELECT * FROM Company WHERE Company_ID ='$Company_ID' ";
+$result = mysqli_query($db, $query); // returns result
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $Company_Thumbnail = $row['Company_Thumbnail'];
+    $Company_Name = $row['Company_Name'];
+
+array_push($return_arr, array(
+    'Company_Thumbnail' => $Company_Thumbnail,
+    'Company_Name' => $Company_Name));
+
+}
+echo json_encode($return_arr); // encodes array as .json and sends back to js 
+
+?>
